be685df7d6fef465af324544d865047736698ea5
Giuliano Portaro <giuliano.portaro@outlook.com>
2019-08-04 12:11:45 +0200

    Leaflet Wrapper for Vue.js

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <HelloWorld/>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/Demo'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
